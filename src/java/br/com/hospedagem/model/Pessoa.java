b0b43ec629f090b6e06d5d823e495af4250cb397
Favero <Favero@10.60.30.105>
2017-06-01 20:12:10 -0300

    Criada unidade de persistencia e entidades (Pessoa e Vaga). OBS: Classe Pessoa falta foto. Classe Vaga falta foto e serviços (lista).

---
+++ src/java/br/com/hospedagem/model/Pessoa.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.hospedagem.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Favero
+ */
+@Entity
+@Table(name="pessoas")
+@SequenceGenerator(name="seq", initialValue=1, allocationSize=1)
+public class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
+    private Long id;
+    
+    @Column(name="nome")
+    private String nome;
+    
+    @Column(name="data_nascimento")
+    private Date dataNasc;
+    
+    @Column(name="telefone")
+    private String telefone;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="profissao")
+    private String profissao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pessoa)) {
+            return false;
+        }
+        Pessoa other = (Pessoa) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.hospedagem.model.Pessoa[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/java/br/com/hospedagem/model/Vaga.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.hospedagem.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Favero
+ */
+@Entity
+@Table(name="vagas")
+@SequenceGenerator(name="seq", initialValue=1, allocationSize=1)
+public class Vaga implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
+    private Long id;
+    
+    @Column(name="titulo")
+    private String titulo;
+    
+    @Column(name="descricao")
+    private String descricao;
+    
+    @Column(name="valor")
+    private double valor;
+    
+    @Column(name="capacidade")
+    private int capacidade;
+    
+    @Column(name="regras")
+    private String regras;
+    
+    @Column(name="localizacao")
+    private String localizacao;
+    
+    @Column(name="reservado")
+    private boolean reservado;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public String getRegras() {
+        return regras;
+    }
+
+    public void setRegras(String regras) {
+        this.regras = regras;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public boolean isReservado() {
+        return reservado;
+    }
+
+    public void setReservado(boolean reservado) {
+        this.reservado = reservado;
+    }
+    
+    
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Vaga)) {
+            return false;
+        }
+        Vaga other = (Vaga) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.hospedagem.model.Vaga[ id=" + id + " ]";
+    }
+    
+}
